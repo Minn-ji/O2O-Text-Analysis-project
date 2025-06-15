63bf51c870f4277a98cb437d73d382e5eed16744
minji choi <cmj04250@gmail.com>
2025-06-15 09:41:12 +0000

    add: code

--- TopicModeling/topic_modeling.py
+++ TopicModeling/topic_modeling.py
@@ -1,63 +1,117 @@
 import pandas as pd
-from gensim import corpora
+import ast
+from gensim import corpora, models
 from kiwipiepy import Kiwi
+import matplotlib.pyplot as plt
 
-def merge_text(df, app_name):
-    df = df[df['sentiment'].isin([7, 8, 9, 10])] # 부정 키워드만 추출
-    merged_text = ''
-    for text in df['text']:
-        if pd.notnull(text):
-            merged_text += str(text) + '. '
-    with open(f'result/{app_name}_review_merged.txt', 'w', encoding='utf-8') as f:
-        f.write(merged_text)
-    print('저장 완료!')
+import gensim
+from gensim.models import CoherenceModel
 
-def load_docs(app_name):
-    with open(f'result/{app_name}_review_merged.txt', 'r', encoding='utf-8') as f:
-        docs = f.read()
-    return docs
-
-def return_merge_df():
-    kakao_taxi_docs = load_docs('kakao_taxi')
-    uber_taxi_docs = load_docs('uber_taxi')
-    yogiyo_docs = load_docs('yogiyo')
-    yeogi_docs = load_docs('yeogi')
-    skyscanner_docs = load_docs('skyscanner')
-    nol_docs = load_docs('nol')
-    df = {
-        'kakao_taxi': kakao_taxi_docs,
-        'uber_taxi': uber_taxi_docs,
-        'yogiyo': yogiyo_docs,
-        'yeogi': yeogi_docs,
-        'skyscanner': skyscanner_docs,
-        'nol': nol_docs
-    }
-
-    df = pd.DataFrame([
-        {'app_name': k, 'review': v}
-        for k, v in df.items()
-    ])
-    df.to_csv('result/merged_review_for_topic_modeling.csv', index=False)
-
-def generate_word_dict_corpus():
-    df = pd.read_csv('result/merged_review_for_topic_modeling.csv')
+def preprocess_reviews(df, app_name):
     kiwi = Kiwi()
-    ALLOW_TAGS = {'NNG', 'SL', 'XR'}
+    df = df[df['sentiment'].isin([7, 8, 9, 10])]
 
-    df['review_preprocessed'] = df['review'].apply(lambda x: kiwi.tokenize(x))
-    df['review_preprocessed'] = df['review_preprocessed'].apply(
+    ALLOW_TAGS = {'NNG', 'VA', 'VV', 'XR', 'MAG'}
+
+    df['tokenized_text'] = df['text'].dropna().apply(lambda x: kiwi.tokenize(x))
+    df['tokenized_text'] = df['tokenized_text'].apply(
         lambda tokens: [token.form for token in tokens if token.tag in ALLOW_TAGS]
     )
+    # 너무 짧은 문장 제거
+    df = df[df['tokenized_text'].apply(lambda x: len(x) >= 2)].copy()
 
-    # 2. 전처리: 너무 짧은 문장 제거
-    df = df[df['review_preprocessed'].apply(lambda x: len(x) >= 2)].copy()
+    df['app_name'] = app_name
+    return df[['app_name', 'tokenized_text']]
 
-    # 3. Dictionary & Corpus 생성
-    word_dict = corpora.Dictionary(df['review_preprocessed'])
+def return_merge_df(kakao_df, uber_df, yogiyo_df, yeogi_df, skyscanner_df, nol_df):
+    dfs = [
+        preprocess_reviews(kakao_df, 'kakao_taxi'),
+        preprocess_reviews(uber_df, 'uber_taxi'),
+        preprocess_reviews(yogiyo_df, 'yogiyo'),
+        preprocess_reviews(yeogi_df, 'yeogi'),
+        preprocess_reviews(skyscanner_df, 'skyscanner'),
+        preprocess_reviews(nol_df, 'nol')
+    ]
+
+    # 전체 통합
+    final_df = pd.concat(dfs, ignore_index=True)
+    final_df.to_csv('result/merged_review_for_topic_modeling.csv', index=False)
+
+
+def generate_word_dict_corpus():
+    print('시작')
+    df = pd.read_csv('result/merged_review_for_topic_modeling.csv')
+    df['tokenized_text'] = df['tokenized_text'].apply(ast.literal_eval)
+    print('## 리스트로 반환: ', type(df['tokenized_text'][0]))
+    
+    # dict / corpus 생성
+    word_dict = corpora.Dictionary(df['tokenized_text'])
     word_dict.filter_extremes(no_below=2, no_above=0.95)  # 단어 수 기준 필터
     word_dict.save('result/word_dict.dict')
-    # word_dict = corpora.Dictionary.load('result/word_dict.dict')
-    corpus = [word_dict.doc2bow(review) for review in df['review_preprocessed']]
+    corpus = [word_dict.doc2bow(review) for review in df['tokenized_text']]
     corpora.MmCorpus.serialize('result/corpus.mm', corpus)
+
+    # 불러올 때
+    # word_dict = corpora.Dictionary.load('result/word_dict.dict')
     # corpus = corpora.MmCorpus('result/corpus.mm')
-    print('생성 및 저장 완료! \n', [(idx,word) for idx, word in word_dict.items()])
+    print('## word dict & corpus 생성 및 저장 완료! \n', [(idx,word) for idx, word in word_dict.items()])
+
+
+def get_topic_ratio_for_each_document():
+    df = pd.read_csv('result/merged_review_for_topic_modeling.csv')
+    df['tokenized_text'] = df['tokenized_text'].apply(ast.literal_eval)
+    app_names = df['app_name'].tolist()
+
+    word_dict = corpora.Dictionary.load('result/word_dict.dict')
+    corpus = corpora.MmCorpus('result/corpus.mm')
+    N_TOPICS = 10
+    ldamodel = gensim.models.ldamodel.LdaModel(
+        corpus, num_topics=N_TOPICS, id2word=word_dict
+    )
+    ldamodel.save('result/lda_model')
+    results = []
+    dominant_topics = []
+    for i, topic_list in enumerate(ldamodel[corpus]):
+        sorted_topic_list = sorted(topic_list, key = lambda x: x[1], reverse=True)
+        most_important_topic, most_important_ratio = sorted_topic_list[0]
+        print(f"문서 {i}의 주요 토픽은 {most_important_topic}번이며, 비중은 {most_important_ratio * 100:.2f}%")
+        print(f"{i}번째 문서의 topic 비중: {topic_list}")
+        results.append([i, app_names[i], most_important_topic, most_important_ratio, sorted_topic_list])
+        dominant_topics.append((app_names[i], most_important_topic))
+    
+    df_result = pd.DataFrame(results, columns=['문서 번호', 'app_name', '주요 토픽', '비중', '전체 토픽 분포'])
+    df_result.to_csv('result/topic_modeling_describe.csv', index=False)
+
+    print("\n토픽별 키워드 ============================")
+    for i in range(N_TOPICS):
+        terms = ldamodel.show_topic(i, topn=10)
+        print(f"토픽 {i}: {[term for term, prob in terms]}")
+    
+    topic_df = pd.DataFrame(dominant_topics, columns=['app_name', 'topic'])
+    topic_dist = topic_df.groupby(['app_name', 'topic']).size().unstack(fill_value=0)
+    topic_ratio = topic_dist.div(topic_dist.sum(axis=1), axis=0)
+    topic_ratio.to_csv('result/topic_ratio_by_app.csv')
+    print('\n앱별 토픽 비율 저장 완료: result/topic_ratio_by_app.csv')
+    print('문서별 토픽 정보 저장 완료!: result/topic_modeling_describe.csv')
+
+
+def find_best_topic_count():
+    df = pd.read_csv('result/merged_review_for_topic_modeling.csv')
+    df['tokenized_text'] = df['tokenized_text'].apply(ast.literal_eval)
+
+    word_dict = corpora.Dictionary.load('result/word_dict.dict')
+    corpus = corpora.MmCorpus('result/corpus.mm')
+    topics_range = range(10, 55, 2)
+    scores = []
+
+    for k in topics_range:
+        ldamodel = models.LdaModel(corpus, num_topics=k, id2word=word_dict)
+        cm = CoherenceModel(model=ldamodel, texts=df['tokenized_text'], dictionary=word_dict, coherence='c_v')
+        scores.append(cm.get_coherence())
+
+    plt.plot(topics_range, scores)
+    plt.xlabel("Number of Topics")
+    plt.ylabel("Coherence Score (C_v)")
+    plt.title("Optimal Number of Topics based on C_v")
+    plt.savefig('result/number_of_topic_graph.png')
+    print("Coherence 그래프 저장 완료")
--- runner.py
+++ runner.py
@@ -2,72 +2,81 @@ import pandas as pd
 
 from util.basic_tools import *
 from util.text_preprocessing_util import preprocess_DataFrame
-# from CrawlReviews.crawl_with_selenium import crawl_google_store_review_with_selenium
-# from CrawlReviews.crawl_with_scraper import crawl_google_store_review_with_scraper
+from CrawlReviews.crawl_with_selenium import crawl_google_store_review_with_selenium
+from CrawlReviews.crawl_with_scraper import crawl_google_store_review_with_scraper
 from SentimentAnalysis.sentiment_model_based import make_json_file, make_sentiment_columns, replace_sentiment_label_to_score
 from WordCloud.draw_wordCloud import generate_wordcloud
-# from WordCloud.draw_wordCloud_sw import generate_wordcloud_sw
-from TopicModeling.topic_modeling import merge_text, generate_word_dict_corpus
+from WordCloud.draw_wordCloud_sw import generate_wordcloud_sw
+from TopicModeling.topic_modeling import return_merge_df, generate_word_dict_corpus, get_topic_ratio_for_each_document, find_best_topic_count
 
-#def scraper_crawl_google_store_review(app_package, app_name):
+def scraper_crawl_google_store_review(app_package, app_name):
     # 구글 스토어 스크래퍼
-    # crawl_google_store_review_with_scraper(app_package, app_name)
+    crawl_google_store_review_with_scraper(app_package, app_name)
 
-# def selenium_crawl_google_store_review(url, app_name):
-#     # 셀레니움 구글 스토어
-#     crawl_google_store_review_with_selenium(url, app_name)
+def selenium_crawl_google_store_review(url, app_name):
+    # 셀레니움 구글 스토어
+    crawl_google_store_review_with_selenium(url, app_name)
 
-# 여기부터 실행
 def preprocess_review_df(df, app_name):
-    # 자동 저장됨
     typo_dict = load_typo_dict()
     kiwi = load_kiwi()
     preprocess_DataFrame(df, kiwi, typo_dict, app_name)
 
 def get_sentiment_column(app_name):
-    # tokenizer = load_tokenizer()
-    # model = load_model()
-    # device = load_device()
-    # make_sentiment_columns(pre_processed_df, tokenizer, model, device, app_name)
+    tokenizer = load_tokenizer()
+    model = load_model()
+    device = load_device()
+    make_sentiment_columns(pre_processed_df, tokenizer, model, device, app_name)
+    # 모델 성능 측정
     senti_added_df = pd.read_csv(f'result/{app_name}_sentiment_analyzed_with_model.csv')
-    replace_sentiment_label_to_score(senti_added_df) # 성능 보여줌
+    replace_sentiment_label_to_score(senti_added_df)
 
-# def get_sentiment_analysis_result_with_model(pre_processed_df, app_name):
-#     # json으로 결과 보기 위한 용도. 안 돌려도 됨
-#     tokenizer = load_tokenizer()
-#     model = load_model()
-#     device = load_device()
-#     sentiment_map = get_sentiment_map()
-#     result_dict = make_json_file(pre_processed_df, tokenizer, model, device, sentiment_map)
-#     save_json(f"result/{app_name}_sentiment_result.json", result_dict)
+def get_sentiment_analysis_result_with_model(pre_processed_df, app_name):
+    # json으로 결과 보기 위한 용도. 안 돌려도 됨
+    tokenizer = load_tokenizer()
+    model = load_model()
+    device = load_device()
+    sentiment_map = get_sentiment_map()
+    result_dict = make_json_file(pre_processed_df, tokenizer, model, device, sentiment_map)
+    save_json(f"result/{app_name}_sentiment_result.json", result_dict)
 
 def get_wordcloud(preprocessed_df, app_name):
     generate_wordcloud(preprocessed_df, app_name)
     # generate_wordcloud_sw(preprocessed_df, app_name)
 
-def merge_reviews_for_topic_modeling(df, app_name):
-    merge_text(df, app_name)
+def merge_reviews_for_topic_modeling(kakao_df, uber_df, yogiyo_df, yeogi_df, skyscanner_df, nol_df):
+    return_merge_df(kakao_df, uber_df, yogiyo_df, yeogi_df, skyscanner_df, nol_df)
 
-def set_dict_corpus_for_topic_modeling():
+def topic_modeling_():
     generate_word_dict_corpus()
+    get_topic_ratio_for_each_document()
+    find_best_topic_count()
 
 if __name__ == '__main__': # python -m runner로 실행 (모듈로)
-    # preprocessed_kakao = pd.read_csv('assets/kakao_taxi_store_merged_scraper_10years_preprocessed.csv')
-    # get_sentiment_column('skyscanner')
-    # get_wordcloud(preprocessed_kakao, 'kakao_taxi')
-    #
-    # preprocessed_uber = pd.read_csv('assets/uber_taxi_store_merged_scraper_10years_preprocessed.csv')
-    # get_wordcloud(preprocessed_uber, 'uber_taxi')
+    
+    ###---- [예시 데이터 : kakao_taxi] ----###
+    ## 1. 크롤링
+    app_package, app_name = 'com.kakao.taxi', 'kakao_taxi'
+    scraper_crawl_google_store_review(app_package, app_name)
 
-    # preprocessed_skyscanner = pd.read_csv('assets/skyscanner_reviews_korean_last.csv')
-    # get_wordcloud(preprocessed_skyscanner, 'skyscanner')
+    ## 2. 전처리
+    kakao_raw_df = pd.read_csv(f'result/{app_name}_google_store_scraper_10years.csv')
+    preprocess_review_df(kakao_raw_df, app_name=app_name)
 
-    # kakao_df = pd.read_csv('result/kakao_taxi_sentiment_analyzed_with_model.csv')
-    # uber_df = pd.read_csv('result/uber_taxi_sentiment_analyzed_with_model.csv')
-    # yogiyo_df = pd.read_csv('result/yogiyo_sentiment_analyzed_with_model.csv')
-    # skyscanner_df = pd.read_csv('result/skyscanner_sentiment_analyzed_with_model.csv')
-    # yeogi_df = pd.read_csv('result/yeogi_sentiment_analyzed_with_model.csv')
-    # nol_df = pd.read_csv('result/nol_sentiment_analyzed_with_model.csv')
-    # merge_reviews_for_topic_modeling(kakao_df, 'kakao_taxi')
-    set_dict_corpus_for_topic_modeling()
+    ## 3-1. 모델 기반 감성 분석 (사전 기반은 도메인 내부에서 실행)
+    get_sentiment_column(app_name=app_name)
+    
+    ## 3-2. 워드클라우드 생성
+    preprocessed_kakao_df = pd.read_csv(f'result/{app_name}_google_store_scraper_10years_preprocessed.csv')
+    get_wordcloud(preprocessed_kakao_df, app_name=app_name)
+
+    ## 3-3. 토픽 모델링 word_dict, corpus, trained lda model 생성 (kakao 포함 전부 감정분석이 완료되어야 수행 가능)(결과는 ipynb에서 확인)
+    kakao_df = pd.read_csv('result/kakao_taxi_sentiment_analyzed_with_model.csv')
+    uber_df = pd.read_csv('result/uber_taxi_sentiment_analyzed_with_model.csv')
+    yogiyo_df = pd.read_csv('result/yogiyo_sentiment_analyzed_with_model.csv')
+    skyscanner_df = pd.read_csv('result/skyscanner_sentiment_analyzed_with_model.csv')
+    yeogi_df = pd.read_csv('result/yeogi_sentiment_analyzed_with_model.csv')
+    nol_df = pd.read_csv('result/nol_sentiment_analyzed_with_model.csv')
+    merge_reviews_for_topic_modeling(kakao_df, uber_df, yogiyo_df, yeogi_df, skyscanner_df, nol_df)
+    topic_modeling_()
 
